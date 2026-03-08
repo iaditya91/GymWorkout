825bfb89f36dea29bea61ba7eb3f281ea06c4815
iaditya91 <iaditya91@gmail.com>
2026-03-08 04:21:49 -0400

    fixed notification

--- app/src/main/java/com/example/gymworkout/notification/ReminderReceiver.kt
+++ app/src/main/java/com/example/gymworkout/notification/ReminderReceiver.kt
@@ -17,38 +17,46 @@ class ReminderReceiver : BroadcastReceiver() {
         val reminderId = intent.getIntExtra(EXTRA_REMINDER_ID, 0)
         val timeIndex = intent.getIntExtra(EXTRA_TIME_INDEX, 0)
 
-        val categoryEnum = try {
-            NutritionCategory.valueOf(category)
-        } catch (_: Exception) {
-            null
-        }
+        val pendingResult = goAsync()
 
-        val title = if (categoryEnum != null) {
-            "${categoryEnum.label} Reminder"
-        } else {
-            "$category Reminder"
-        }
-        val text = customText.ifBlank {
-            when (categoryEnum) {
-                NutritionCategory.WATER -> "Time to drink water!"
-                NutritionCategory.CARBS -> "Don't forget your carbs intake!"
-                NutritionCategory.PROTEIN -> "Time for your protein!"
-                NutritionCategory.VITAMINS -> "Remember to take your vitamins!"
-                NutritionCategory.SLEEP -> "Time to prepare for sleep!"
-                null -> "Don't forget your $category!"
-            }
-        }
-
-        NotificationHelper.createNotificationChannel(context)
-        val notificationId = reminderId * 100 + timeIndex
-        NotificationHelper.showNotification(context, notificationId, title, text)
-
-        // Reschedule for next day
         CoroutineScope(Dispatchers.IO).launch {
-            val dao = WorkoutDatabase.getDatabase(context).reminderDao()
-            val reminder = dao.getReminderById(reminderId)
-            if (reminder != null && reminder.enabled) {
-                ReminderScheduler.scheduleReminder(context, reminder)
+            try {
+                val categoryEnum = try {
+                    NutritionCategory.valueOf(category)
+                } catch (_: Exception) {
+                    null
+                }
+
+                val db = WorkoutDatabase.getDatabase(context)
+                val label = if (categoryEnum != null) {
+                    categoryEnum.label
+                } else {
+                    db.nutritionDao().getTargetSync(category)?.label ?: category
+                }
+
+                val title = "$label Reminder"
+                val text = customText.ifBlank {
+                    when (categoryEnum) {
+                        NutritionCategory.WATER -> "Time to drink water!"
+                        NutritionCategory.CARBS -> "Don't forget your carbs intake!"
+                        NutritionCategory.PROTEIN -> "Time for your protein!"
+                        NutritionCategory.VITAMINS -> "Remember to take your vitamins!"
+                        NutritionCategory.SLEEP -> "Time to prepare for sleep!"
+                        null -> "Don't forget your $label!"
+                    }
+                }
+
+                NotificationHelper.createNotificationChannel(context)
+                val notificationId = reminderId * 100 + timeIndex
+                NotificationHelper.showNotification(context, notificationId, title, text)
+
+                // Reschedule for next day
+                val reminder = db.reminderDao().getReminderById(reminderId)
+                if (reminder != null && reminder.enabled) {
+                    ReminderScheduler.scheduleReminder(context, reminder)
+                }
+            } finally {
+                pendingResult.finish()
             }
         }
     }
